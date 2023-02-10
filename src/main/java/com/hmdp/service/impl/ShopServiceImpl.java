a2a23b69665a16c9ef0917fc3596c50cbf08c2ed
yuechuc <304723403@qq.com>
2023-02-11 02:23:35 +0800

    使用互斥锁解决缓存击穿

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
@@ -16,6 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.Resource;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.*;
@@ -40,12 +43,19 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         //使用空对象解决缓存穿透
         // Shop shop = queryWithPassThrough(id);
 
+        // 使用互斥锁解决缓存击穿
+        // Shop shop = queryWithMutex(id);
 
         // 使用互斥锁解决缓存击穿
-        Shop shop = queryWithMutex(id);
+        Shop shop = queryWithLogicalExpire(id);
+
+
+
+
         if (shop == null) {
             return Result.fail("店铺不存在");
         }
+
         return Result.ok(shop);
     }
 
@@ -134,6 +144,54 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         return shop;
     }
 
+    //线程池
+    public static final ExecutorService CACHE_REBUILD_EXECUTE = Executors.newFixedThreadPool(10);
+
+    public Shop queryWithLogicalExpire(Long id){
+        // 1.从redis查询商铺缓存
+        String key = CACHE_SHOP_KEY + id;
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 2.判断是否存在
+        if (StrUtil.isBlank(shopJson)) {
+            // 3.不存在  直接返回
+            return null;
+        }
+        //4.命中，先把json对象反序列化为对象
+        RedisData redisData = JSONUtil.toBean(shopJson, RedisData.class);
+        JSONObject data = (JSONObject) redisData.getData();
+        Shop shop = JSONUtil.toBean(data, Shop.class);
+        LocalDateTime expireTime = redisData.getExpireTime();
+
+        //5.判断是否过期
+        if (expireTime.isAfter(LocalDateTime.now())) {
+            //5.1未过期，直接返回商铺信息
+            return shop;
+        }
+        //5.2已过期，需要缓存重建
+        //6.缓存重建
+        //6.1获取互斥锁
+        String lockKey = LOCK_SHOP_KEY + id;
+        boolean isLock = tryLock(lockKey);
+        //6.2判断是否成功
+        if (isLock) {
+            //6.3成功，开启独立线程，实现缓存重建
+            CACHE_REBUILD_EXECUTE.submit(()->{
+                // 重建缓存
+                try {
+                    this.addLogicalExpire(id, 20L);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }finally {
+                    // 释放锁
+                    unLock(lockKey);
+                }
+            });
+        }
+
+        // 6.4返回过期的商铺信息
+        return shop;
+    }
+
     //添加互斥锁
     private boolean tryLock(String key){
         Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10,TimeUnit.SECONDS);
@@ -144,6 +202,22 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     private void unLock(String key){
        stringRedisTemplate.delete(key);
     }
+
+
+
+    //写入逻辑过期时间
+    public void addLogicalExpire(Long id,Long expireSecond) throws InterruptedException {
+        Shop shop = getById(id);
+        Thread.sleep(200);
+        //写入逻辑过期时间
+        RedisData data = new RedisData();
+        data.setData(shop);
+        data.setExpireTime(LocalDateTime.now().plusSeconds(expireSecond));
+        //写入redis
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY+id,JSONUtil.toJsonStr(data));
+    }
+
+
 
 
 
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,21 @@
 package com.hmdp;
 
+import com.hmdp.service.IShopService;
+import com.hmdp.service.impl.ShopServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
 
+    @Autowired
+    private ShopServiceImpl shopService;
+
+    // 写入逻辑过期时间
+    @Test
+    void addLogicalExpire() throws InterruptedException {
+        shopService.addLogicalExpire(1L, 10L);
+    }
 }
