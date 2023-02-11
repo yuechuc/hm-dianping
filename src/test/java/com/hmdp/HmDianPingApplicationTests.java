deb125a8a8b74a3eca13eafb793d158e7205f745
yuechuc <304723403@qq.com>
2023-02-12 03:31:12 +0800

    全局唯一Id

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -4,12 +4,16 @@ import com.hmdp.entity.Shop;
 import com.hmdp.service.IShopService;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
@@ -17,6 +21,10 @@ import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 @SpringBootTest
 class HmDianPingApplicationTests {
 
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
 
     @Autowired
     private ShopServiceImpl shopService;
@@ -30,5 +38,27 @@ class HmDianPingApplicationTests {
         // shopService.addLogicalExpire(1L, 10L);
         Shop shop = shopService.getById(1L);
         cacheClient.setLogicalExpire(CACHE_SHOP_KEY + 1L,shop,10L, TimeUnit.SECONDS);
+    }
+
+    
+    @Test
+    void idWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = ()->{
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id="+id);
+            }
+            latch.countDown();
+        };
+
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time="+(end-begin));
     }
 }
